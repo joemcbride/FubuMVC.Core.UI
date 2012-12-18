96c40afd4f54e1a464fc58ef17a0836a6bdbb7ba
jeremydmiller <jeremydmiller@yahoo.com>
2012-12-18 13:57:10 -0600

    easy accessor level overrides of Html conventions

---
+++ src/FubuMVC.Core.UI.Testing/Integration/Explicit_accessor_overrides_integration_tester.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using FubuCore;
+using FubuMVC.Core.Registration;
+using FubuMVC.Core.Runtime;
+using FubuMVC.Core.UI.Elements;
+using FubuMVC.TestingHarness;
+using HtmlTags;
+using HtmlTags.Conventions;
+using NUnit.Framework;
+using FubuTestingSupport;
+
+namespace FubuMVC.Core.UI.Testing.Integration
+{
+    [TestFixture]
+    public class Explicit_accessor_overrides_integration_tester : FubuRegistryHarness
+    {
+        protected override void configure(FubuRegistry registry)
+        {
+            registry.Actions.IncludeType<OverrideEndpoint>();
+
+            // The profile has to exist before accessors can work
+            registry.Import<HtmlConventionRegistry>(x => {
+                x.Profile("Green", profile => {
+                    profile.Displays.Always.BuildBy<ComplexBuilder>();
+                });
+            });
+        }
+
+        [Test]
+        public void override_the_display_for_the_default()
+        {
+            endpoints.GetByInput(new OverrideRequest {Category = ElementConstants.Display})
+                .ReadAsText()     
+                .ShouldEqual(new BlueBuilder().Build(null).ToString());
+        }
+
+        [Test]
+        public void override_the_display_for_the_label()
+        {
+            endpoints.GetByInput(new OverrideRequest { Category = ElementConstants.Label })
+                .ReadAsText()
+                .ShouldEqual(new SimpleBuilder().Build(null).ToString());
+        }
+
+        [Test]
+        public void override_the_display_for_the_input()
+        {
+            endpoints.GetByInput(new OverrideRequest { Category = ElementConstants.Editor })
+                .ReadAsText()
+                .ShouldEqual(new ComplexBuilder().Build(null).ToString());
+        }
+
+        [Test]
+        public void override_display_by_profile()
+        {
+            endpoints.GetByInput(new OverrideRequest { Category = ElementConstants.Display, Profile = "Green"})
+                .ReadAsText()
+                .ShouldEqual(new GreenBuilder().Build(null).ToString());
+        }
+    }
+
+    public class TestAccessorOverrides : OverridesFor<OverrideTarget>
+    {
+        public TestAccessorOverrides()
+        {
+            Property(x => x.Name)
+                .DisplayBuilder<BlueBuilder>()
+                .LabelBuilder<SimpleBuilder>()
+                .InputBuilder<ComplexBuilder>()
+                .DisplayBuilder<GreenBuilder>(profile:"Green");
+        }
+    }
+
+    public class OverrideEndpoint
+    {
+        private readonly IElementGenerator<OverrideTarget> _generator;
+
+        public OverrideEndpoint(IElementGenerator<OverrideTarget> generator)
+        {
+            _generator = generator;
+        }
+
+        public HtmlTag get_target_Category_Profile(OverrideRequest request)
+        {
+            var elemRequest = ElementRequest.For<OverrideTarget>(x => x.Name);
+            if (request.Category == ElementConstants.Display)
+            {
+                return _generator.DisplayFor(elemRequest, profile: request.Profile);
+            }
+
+            if (request.Category == ElementConstants.Label)
+            {
+                return _generator.LabelFor(elemRequest, profile: request.Profile);
+            }
+
+            return _generator.InputFor(elemRequest, profile: request.Profile);
+        }
+    }
+
+    public class OverrideRequest
+    {
+        public OverrideRequest()
+        {
+            Profile = TagConstants.Default;
+        }
+
+        public string Category { get; set; }
+        public string Profile { get; set; }
+    }
+
+
+
+    public class OverrideTarget
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+
+    public class SimpleBuilder : IElementBuilder
+    {
+        public HtmlTag Build(ElementRequest request)
+        {
+            return new HtmlTag("div").AddClass("simple");
+        }
+    }
+
+    public class ComplexBuilder : IElementBuilder
+    {
+        public HtmlTag Build(ElementRequest request)
+        {
+            return new HtmlTag("div").AddClass("complex");
+        }
+    }
+
+    public class DifferentBuilder : IElementBuilder
+    {
+        public HtmlTag Build(ElementRequest request)
+        {
+            return new HtmlTag("div").AddClass("different");
+        }
+    }
+
+    public class GreenBuilder : IElementBuilder
+    {
+        public HtmlTag Build(ElementRequest request)
+        {
+            return new HtmlTag("div").AddClass("green");
+        }
+    }
+
+    public class BlueBuilder : IElementBuilder
+    {
+        public HtmlTag Build(ElementRequest request)
+        {
+            return new HtmlTag("div").AddClass("blue");
+        }
+    }
+}
