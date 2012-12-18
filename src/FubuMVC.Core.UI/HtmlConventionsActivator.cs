f9e041b282acf034fad34f8dee2072b8e5796f13
jeremydmiller <jeremydmiller@yahoo.com>
2012-12-18 09:29:08 -0600

    added the insertion of the override builder into each category/profile

--- src/FubuMVC.Core.UI/HtmlConventionsActivator.cs
+++ src/FubuMVC.Core.UI/HtmlConventionsActivator.cs
@@ -1,21 +1,26 @@
 ﻿using System.Collections.Generic;
 using Bottles;
 using Bottles.Diagnostics;
+using FubuCore.Reflection;
 using FubuMVC.Core.Bootstrapping;
 using FubuMVC.Core.Registration;
 using FubuMVC.Core.Registration.ObjectGraph;
+using FubuMVC.Core.UI.Elements;
 using FubuMVC.Core.UI.Forms;
 using HtmlTags.Conventions;
 
 namespace FubuMVC.Core.UI
 {
-    public class HtmlConventionsActivator : IActivator
+    public class HtmlConventionsActivator : IActivator, ITagLibraryVisitor<ElementRequest>
     {
+        private readonly AccessorRules _rules;
         private readonly BehaviorGraph _graph;
         private readonly IContainerFacility _facility;
+        private string _category;
 
-        public HtmlConventionsActivator(BehaviorGraph graph, IContainerFacility facility)
+        public HtmlConventionsActivator(AccessorRules rules, BehaviorGraph graph, IContainerFacility facility)
         {
+            _rules = rules;
             _graph = graph;
             _facility = facility;
         }
@@ -25,8 +30,20 @@ namespace FubuMVC.Core.UI
             var library = _graph.Settings.Get<HtmlConventionLibrary>();
             library.Import(new DefaultHtmlConventions().Library);
 
+            library.For<ElementRequest>().AcceptVisitor(this);
 
             _facility.Register(typeof(HtmlConventionLibrary), ObjectDef.ForValue(library));
+        }
+
+        public void Category(string name, TagCategory<ElementRequest> category)
+        {
+            _category = name;
+        }
+
+        public void BuilderSet(string profile, BuilderSet<ElementRequest> builders)
+        {
+            var policy = new AccessorOverrideElementBuilderPolicy(_rules, _category, profile);
+            builders.InsertFirst(policy);
         }
     }
 }
