87e68052bce6a153bb06ad2ce3f43dde6e1556cc
jmarnold <rncodepoet@gmail.com>
2012-12-28 15:29:09 -0600

    Adds backing tests for the FormRequest service attachment

---
+++ src/FubuMVC.Core.UI.Testing/Forms/FormRequestTester.cs
@@ -0,0 +1,78 @@
+﻿using FubuCore;
+using FubuMVC.Core.Http;
+using FubuMVC.Core.Registration.Nodes;
+using FubuMVC.Core.Registration.Querying;
+using FubuMVC.Core.Registration.Routes;
+using FubuMVC.Core.UI.Forms;
+using FubuTestingSupport;
+using NUnit.Framework;
+using Rhino.Mocks;
+
+namespace FubuMVC.Core.UI.Testing.Forms
+{
+    [TestFixture]
+    public class FormRequestTester
+    {
+        private InMemoryServiceLocator theServices;
+        private ChainSearch theSearch;
+        private FormRequest theRequest;
+        private object theInput;
+
+        private IChainResolver theResolver;
+        private ICurrentHttpRequest theCurrentRequest;
+        private BehaviorChain theChain;
+
+        [SetUp]
+        public void SetUp()
+        {
+            theServices = new InMemoryServiceLocator();
+            theSearch = ChainSearch.ByUniqueInputType(typeof (object));
+            theInput = new object();
+
+            theResolver = MockRepository.GenerateStub<IChainResolver>();
+            theCurrentRequest = MockRepository.GenerateStub<ICurrentHttpRequest>();
+
+            theChain = new BehaviorChain();
+
+            theServices.Add(theResolver);
+            theServices.Add(theCurrentRequest);
+
+            theRequest = new FormRequest(theSearch, theInput);
+        }
+
+        [Test]
+        public void throws_if_the_chain_cannot_be_found()
+        {
+            Exception<FubuException>.ShouldBeThrownBy(() => theRequest.Attach(theServices));
+        }
+
+        [Test]
+        public void throws_if_the_route_is_null()
+        {
+            theChain.Route = null;
+            theResolver.Stub(x => x.Find(theSearch)).Return(theChain);
+
+            Exception<FubuException>.ShouldBeThrownBy(() => theRequest.Attach(theServices));
+        }
+
+        [Test]
+        public void sets_the_full_url()
+        {
+            var fullUrl = "this/is/a/test";
+
+            var route = new RouteDefinition("blah");
+            route.ApplyInputType(typeof(object));
+            
+            theChain.Route = route;
+
+            var url = theChain.Route.CreateUrlFromInput(theInput);
+            
+            theCurrentRequest.Stub(x => x.ToFullUrl(url)).Return(fullUrl);
+            theResolver.Stub(x => x.Find(theSearch)).Return(theChain);
+
+            theRequest.Attach(theServices);
+
+            theRequest.Url.ShouldEqual(fullUrl);
+        }
+    }
+}
